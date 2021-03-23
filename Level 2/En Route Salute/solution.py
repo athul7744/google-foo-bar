2dbd1947c244b71d41d164e63cd83871311f5e4a
Athul Anil Kumar <athul7744@outlook.com>
2021-03-23 21:33:03 +0530

    Update solution.py

--- Level 2/En Route Salute/solution.py
+++ Level 2/En Route Salute/solution.py
@@ -1,1 +1,20 @@
 def solution(s):
+  
+  h = {}
+  #separating the indexes of minions moving right and left separately
+  h['>'] = []
+  h['<'] = []
+  salutes = 0
+  
+  #storing the indexes
+  for i,e in enumerate(s):
+    if e == '>' or e == '<':
+      h[e].append(i)
+      
+  #if right moving index is lesser than left moving index, increase salutes by 2
+  for x in h['>']:
+    for y in h['<']:
+      if x < y:
+        salutes += 2
+        
+  return salutes
