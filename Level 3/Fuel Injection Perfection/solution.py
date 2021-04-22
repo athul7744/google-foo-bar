97eb8026c22e916c8abb758465d4d13141bc511a
Athul Anil Kumar <athul7744@outlook.com>
2021-04-22 21:58:53 +0530

    Update solution.py

--- Level 3/Fuel Injection Perfection/solution.py
+++ Level 3/Fuel Injection Perfection/solution.py
@@ -12,7 +12,7 @@ def solution(n):
             val += 1
         #If a number is odd, then we can either reduce or increase it to make it even so that we can divide by 2 in the next step
         #But branching in both cases to find the solution is not always optimal, we can find an optimal solution in constant time
-        #If a number has remainder of 1 on dividing by 4 of if number is 3, the best option is to subract 1
+        #If a number has remainder of 1 on dividing by 4 or if number is 3, the best option is to subract 1
         #Else the best option is to add 1
         #This can be observed by taking the last 3 bits of the odd number (001,101,011,111) - all possible combinations
         #By adding or subracting 1 and dividing accordingly we can notice that the values converge faster when we follow the rules above.
